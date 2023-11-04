d58ff537805da615145a9c3163ba3a1448405ca7
HenryCaicedo <72535346+HenryCaicedo@users.noreply.github.com>
2023-11-04 14:45:57 -0500

    Sidebar mejorada

--- src/components/UnitContainer.jsx
+++ src/components/UnitContainer.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import Lesson from "./Lessons";
 const activitiesFolderPath = "../game"; // Replace with the actual path
 
-function UnitContainer({ title, color='sky-500'}) {
+function UnitContainer({ title, color='purple-500'}) {
   const jsFiles = getJSFilesInFolder(activitiesFolderPath);
 
   function getJSFilesInFolder(folderPath) {
@@ -24,41 +24,9 @@ function UnitContainer({ title, color='sky-500'}) {
     );
   };
 
-  color = 'bg-'+color;
-  //color = 'bg-orange-400';
-  console.log(color);
 
-
-
-  /*
-  return (
-    <div className="UnitContainer flex space-y-[15vh] pt-[5vh] relative left-[70vh] w-[100vh] h-full bg-yellown p-5 m-5 shadow rounded-xl flex flex-col overflow-hidden">
-      <h2 className="text-bluen text-6xl font-bold font-['Open Sans'] drop-shadow">
-        {title}
-      </h2>
-      <div className="ButtonRow space-x-4 space">
-        <Link to={`/app`}>
-          <div className="space-y-4 relative left-[7vh] top-[-7vh]">
-            <div className="flex space-x-4 relative left-[9vh]">
-              <Lesson number={1} />
-              <Lesson number={2} />
-              <Lesson number={3} />
-              <Lesson number={4} />
-            </div>
-            <div className="flex space-x-4  relative left-[9vh]">
-              <Lesson number={5} />
-              <Lesson number={6} />
-              <Lesson number={7} />
-              <Lesson number={8} />
-            </div>
-          </div>
-        </Link>
-      </div>
-    </div>
-
-    
-  );
-  */
+  var str = "bg-sky-500 rounded-3xl py-5 px-7";
+  console.log(str);
 
   return (
 
@@ -67,7 +35,7 @@ function UnitContainer({ title, color='sky-500'}) {
       <div className="flex items-center justify-center">
 
         <div className="flex border shadow-md p-10 flex-col space-y-2 rounded-[45px] bg-zinc-50">
-          <div className={`${color} rounded-3xl py-5 px-7`}>
+          <div className={str}>
             <h2 className="text-white text-2xl font-semibold font-['Open Sans']">
               {title}
             </h2>
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,37 +1,58 @@
-import { MoreVertical, ChevronLast, ChevronFirst, BoxIcon } from "lucide-react";
+import { CarFront, LogOutIcon } from "lucide-react";
+import { Icon } from '@iconify/react';
 import React from "react";
 
-function Tab({ icon, label }) {
-  return (
-    <div className="p-4 flex items-center">
-      <div className="text-3xl text-black mr-4">{icon}</div> {/* Use the passed icon as a component with larger black text */}
-      <div className="text-lg font-semibold text-black">{label}</div> {/* Use the passed label as the word with larger black text */}
-    </div>
-  );
-}
+
 
 
 function SidebarHeader() {
   return (
-    <div className="p-4 flex items-center">
-      <div className="text-3xl text-black mr-4"><BoxIcon className="mr-2" /></div>
-      <div className="text-lg font-semibold text-black">Application</div> 
+    <div className="p-4 flex items-center w-full space-x-2">
+      <div className="text-black mr-4" size={50}><CarFront color="darkblue" size={42} /></div>
+      <div className="text-2xl font-semibold text-blue-900">AppName</div>
     </div>
   );
 }
 
 
+function Tab({ icon, label }) {
+  return (
+    <button className="px-4 py-3 flex items-center w-full
+      hover:bg-zinc-200 hover:rounded-lg space-x-2">
+      <div className="text-[4.5vh] text-black mr-4">{icon}</div> {/* Use the passed icon as a component with larger black text */}
+      <div className="text-xl font-semibold text-black">{label}</div> {/* Use the passed label as the word with larger black text */}
+    </button>
+  );
+}
+
 
 function Sidebar({ profileImage, settingsImage }) {
   return (
-    <div className="h-full w-full space-y-24 py-32 pl-14">
-      <div className="space-y-2">
-        <SidebarHeader/>
-        <Tab icon={<MoreVertical className="mr-2" />} label="Tab" />
-        <Tab icon={<ChevronFirst className="mr-2" />} label="Settings" />
+    <div className="h-full w-full py-24 pl-14">
+      <div className="flex flex-col justify-between h-full ">
+
+
+        <div className="space-y-2 ">
+          <div className="h-1/6  w-full flex flex-col justify-center border-b-2 border-gray-200 pb-14 pt-5">
+            <SidebarHeader />
+          </div>
+          <div className="px-5">
+            <Tab icon={<Icon icon="flat-color-icons:businesswoman" />} label="My account" />
+            <Tab icon={<Icon icon="flat-color-icons:graduation-cap" />} label="Learn" />
+            <Tab icon={<Icon icon="flat-color-icons:settings" />} label="Settings" />
+          </div>
+
+        </div>
+
+        <div className="border-t-2 border-gray-200">
+          <div className="pt-2" >
+            <Tab icon={<LogOutIcon className="mr-2 text-red-500" />} label="Log out" />
+          </div>
+
+        </div>
       </div>
-      
-      <Tab icon={<ChevronLast className="mr-2" />} label="Lol" />
+
+
     </div>
   );
 }
