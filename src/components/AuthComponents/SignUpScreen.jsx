cef67bddb569453ed4c22c251f6418d1f8aec29f
Felipe De Lima Correa <61438325+d3l1m@users.noreply.github.com>
2023-11-09 18:54:03 -0500

    Changes to signup

--- src/components/AuthComponents/SignUpScreen.jsx
+++ src/components/AuthComponents/SignUpScreen.jsx
@@ -1,27 +1,94 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import AuthNavBar from './AuthNavBar';
-import TextField from './TextField';
-import { MainButton } from '../Theme/ThemeComponents';
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthNavBar from "./AuthNavBar";
+import TextField from "./TextField";
+import axios from "axios"; // Import Axios
+import { MainButton } from "../Theme/ThemeComponents";
 export default function SignUp() {
-    return (
-        <div>
-            <AuthNavBar login={false}/>
-            <div className="flex items-center justify-center h-[80vh]">
+  const [formData, setFormData] = useState({
+    nombre: "",
+    correoElectronico: "",
+    contraseña: "",
+  });
+  const [isFailure, setIsFailure] = useState(false);
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const navigate = useNavigate();
+  const handleSignUp = async () => {
+    try {
+      console.log(JSON.stringify(formData));
+      const { confirmPassword, ...formDataWithoutConfirm } = formData;
 
-                <div className="bg-white flex flex-col space-y-5 items-center justify-center text-white w-[40vh] font-bold rounded-[4vh] border-[3px] border-gray-300 shadow-lg p-10">
-                    <div className="bg-white w-[15vh] h-[15vh] rounded-full absolute top-[16vh] border-[3px] border-gray-300 shadow-lg"></div>
-                    <TextField placeholder={"Nombre"}/>
-                    <TextField placeholder={"Email"}/>
-                    <TextField placeholder={"Contraseña"}/>
-                    <TextField placeholder={"Confirmar contraseña"}/>
-                    <Link to="/progress" className='w-full'>  
-                        <MainButton>Crear cuenta</MainButton>
-                    </Link>
-                </div>
+      console.log(JSON.stringify(formDataWithoutConfirm));
+      const response = await axios.post("http://localhost:8080/signup", {
+        nombre: formData.nombre,
+        correoElectronico: formData.correoElectronico,
+        contraseña: formData.contraseña,
+      });
 
+      // Check the result of the request
+      console.log(response.status);
+      if (response.status == 201) {
+        // response.data.success returns undefined.
+        console.log("SignUp successful!");
+        navigate("/progress");
+      } else {
+        console.log(response.data);
+      }
+    } catch (error) {
+      // Handle errors
+      setIsFailure(true);
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <div>
+      <AuthNavBar login={false} />
+      <div className="flex items-center justify-center h-[80vh]">
+        <div className="bg-white flex flex-col space-y-5 items-center justify-center text-white w-[40vh] font-bold rounded-[4vh] border-[3px] border-gray-300 shadow-lg p-10">
+          <div className="bg-white w-[15vh] h-[15vh] rounded-full absolute top-[16vh] border-[3px] border-gray-300 shadow-lg"></div>
+          <TextField
+            placeholder={"Nombre"}
+            name="nombre"
+            value={formData.nombre}
+            onChange={(value) => handleInputChange("nombre", value)}
+          />
+          <TextField
+            placeholder={"Email"}
+            name="correoElectronico"
+            value={formData.correoElectronico}
+            onChange={(value) => handleInputChange("correoElectronico", value)}
+          />
+          <TextField
+            placeholder={"Contraseña"}
+            type="password"
+            name="contraseña"
+            value={formData.contraseña}
+            onChange={(value) => handleInputChange("contraseña", value)}
+          />
+          <TextField
+            placeholder={"Confirmar contraseña"}
+            type="password"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={(value) => handleInputChange("confirmPassword", value)}
+          />
+
+          <div onClick={handleSignUp} className="w-full">
+            <MainButton>Crear cuenta</MainButton>
+          </div>
+          {isFailure ? (
+            <div className="bg-black">
+              Ha ocurrido un error al crear el usuario.
             </div>
+          ) : null}
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/AuthComponents/TextField.jsx
+++ src/components/AuthComponents/TextField.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function TextField({placeholder="textfield"}) {
+export default function TextField({
+  placeholder = "textfield",
+  name,
+  value,
+  onChange,
+}) {
+  const handleInputChange = (e) => {
+    // Use e.target.value to get the input value
+    onChange(e.target.value);
+  };
+
   return (
-        <input type="text" placeholder={placeholder} className='w-full rounded-2xl p-2 bg-gray-200 text-black font-semibold'/>
-  )
+    <input
+      type="text"
+      placeholder={placeholder}
+      onChange={handleInputChange}
+      className="w-full rounded-2xl p-2 bg-gray-200 text-black font-semibold"
+    />
+  );
 }
