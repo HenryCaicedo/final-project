f9c300d33ecc5219f98dabc6b3db85837be9a99e
Felipe De Lima Correa <61438325+d3l1m@users.noreply.github.com>
2023-11-13 19:49:32 -0500

    Update Instruction.jsx

--- src/game/Instruction.jsx
+++ src/game/Instruction.jsx
@@ -1,36 +1,41 @@
-// Instruction.jsx
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect, useRef } from "react";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { Trash2, Minus, Plus, ChevronsRight, CornerUpRight, CornerUpLeft, Eye, GripHorizontal } from "lucide-react";
-import InstructionsContext, { InstructionsProvider } from "./InstructionsProvider";
-
-
+import {
+  Trash2,
+  Minus,
+  Plus,
+  ChevronsRight,
+  CornerUpRight,
+  CornerUpLeft,
+  Eye,
+  GripHorizontal,
+} from "lucide-react";
+import InstructionsContext, {
+  InstructionsProvider,
+} from "./InstructionsProvider";
 
 const instructionDictionary = {
   forward: {
-    translation: 'Avanzar',
+    translation: "Avanzar",
     icon: <ChevronsRight />,
   },
   right: {
-    translation: 'Derecha',
+    translation: "Derecha",
     icon: <CornerUpRight />,
   },
   left: {
-    translation: 'Izquierda',
+    translation: "Izquierda",
     icon: <CornerUpLeft />,
   },
   wait: {
-    translation: 'Observar',
+    translation: "Observar",
     icon: <Eye />,
   },
 };
 
-
-
-
-function ValueCounter({ value, instructionId }) {
-  const style = 'hover:bg-gray-300 rounded';
+function ValueCounter({ value, instructionId, onMouseEnter, onMouseLeave }) {
+  const style = "hover:bg-gray-300 rounded";
   const { updateInstructionValue } = useContext(InstructionsContext);
 
   const handleIncrement = () => {
@@ -42,7 +47,11 @@ function ValueCounter({ value, instructionId }) {
   };
 
   return (
-    <div className="flex w-fit items-center border-2 rounded-lg space-x-2">
+    <div
+      className="flex w-fit items-center border-2 rounded-lg space-x-2"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
       <button className={style} onClick={handleDecrement}>
         <Minus />
       </button>
@@ -54,19 +63,26 @@ function ValueCounter({ value, instructionId }) {
   );
 }
 
-
 function Instruction({ user: instruction }) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: instruction.id });
+  const buttonRef = useRef(null);
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
+  const {
+    attributes: sortableAttributes,
+    listeners: sortableListeners,
+    setNodeRef: sortableSetNodeRef,
+    transform,
+    transition,
+  } = useSortable({ id: instruction.id });
 
   const style = {
     transform: CSS.Transform.toString(transform),
     transition,
   };
 
-
-
-  const translatedInstruction = instructionDictionary[instruction.type] || { translation: instruction.type, icon: null };
+  const translatedInstruction = instructionDictionary[instruction.type] || {
+    translation: instruction.type,
+    icon: null,
+  };
 
   const translatedInstructionType = translatedInstruction.translation;
   const icon = translatedInstruction.icon;
@@ -75,53 +91,76 @@ function Instruction({ user: instruction }) {
 
   const handleTrashCanClick = (instructionId) => {
     removeInstruction(instructionId);
-  }
+  };
+
+  const handleMouseEnter = () => {
+    setIsButtonHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsButtonHovered(false);
+  };
+
+  useEffect(() => {
+    const changeInstruction = buttonRef.current;
+
+    if (changeInstruction) {
+      changeInstruction.addEventListener("mouseenter", handleMouseEnter);
+      changeInstruction.addEventListener("mouseleave", handleMouseLeave);
+    }
+
+    return () => {
+      if (changeInstruction) {
+        changeInstruction.removeEventListener("mouseenter", handleMouseEnter);
+        changeInstruction.removeEventListener("mouseleave", handleMouseLeave);
+      }
+    };
+  }, [handleMouseEnter, handleMouseLeave]);
+
+  const { setNodeRef, attributes, listeners } = isButtonHovered
+    ? { setNodeRef: () => {}, attributes: {}, listeners: {} }
+    : {
+        setNodeRef: sortableSetNodeRef,
+        attributes: sortableAttributes,
+        listeners: sortableListeners,
+      };
 
   return (
-    <div
-      style={style}
-      ref={setNodeRef}
-      {...attributes}
-      {...listeners}
-    >
-
-      <div id='main' className={"flex bg-white rounded-2xl py-2 px-5 h-[50px] font-semibold text-lg items-center border-[3px] hover:bg-gray-100 hover:border-gray-200 overflow-hidden relative"}>
-
-
-        <div className="w-[10%]">
-          {icon}
-        </div>
+    <div style={style} ref={setNodeRef} {...attributes} {...listeners}>
+      <div
+        className={
+          "flex bg-white rounded-2xl py-2 px-5 h-[50px] font-semibold text-lg items-center border-[3px] hover:bg-gray-100 hover:border-gray-200 overflow-hidden relative"
+        }
+      >
+        <div className="w-[10%]">{icon}</div>
 
         <div className="w-[55%] overflow-visible pl-3">
-
-          {instruction.type === 'forward' ? (
-            <ValueCounter value={instruction.value} instructionId={instruction.id}/>
+          {instruction.type === "forward" ? (
+            <ValueCounter
+              value={instruction.value}
+              instructionId={instruction.id}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
           ) : (
             <h1 className="flex-grow">{translatedInstructionType}</h1>
           )}
-
         </div>
 
-        <div className="w-[20%] pr-2 bg-pink-200">
-
-        </div>
+        <div className="w-[20%] pr-2 bg-pink-200"></div>
 
         <div className="w-[15%] flex items-center justify-center">
-          <button className="hover:bg-red-300 rounded-lg p-1"
-          onClick={() => handleTrashCanClick(instruction.id)}
-
+          <button
+            className="hover:bg-red-300 rounded-lg p-1"
+            onClick={() => handleTrashCanClick(instruction.id)}
+            ref={buttonRef}
           >
             <Trash2 size={24} />
           </button>
         </div>
-
       </div>
-
-
-
-    </div >
+    </div>
   );
-
 }
 
 export default Instruction;
