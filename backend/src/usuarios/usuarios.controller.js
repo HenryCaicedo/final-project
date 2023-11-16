6f4aba8f1e86aa28050822faad9d59508d5772c5
Zenemi44 <107159982+Zenemi44@users.noreply.github.com>
2023-11-16 09:53:17 -0500

    login fix

--- backend/src/usuarios/usuarios.controller.js
+++ backend/src/usuarios/usuarios.controller.js
@@ -43,10 +43,11 @@ export async function loginUsuario(req, res) {
     if (!passwordMatch) {
       return res.status(401).json({ message: 'Invalid password.' });
     }
+    const id=user._id;
 
     const token = jwt.sign({ userId: user._id }, process.env.AUTH_TOKEN);
 
-    return res.status(200).json({ token });
+    return res.status(200).json({ token, id });
   } catch (error) {
     return res.status(500).json({ message: 'Server Error' });
   }
